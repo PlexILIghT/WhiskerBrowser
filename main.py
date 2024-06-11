3ee4ecf8e5da43ea3528819c2421d3eb1783dadf
LiquidDevQ <139150402+LiquidDevQ@users.noreply.github.com>
2024-06-11 22:43:24 +1000

    added almost everything except bookmarks

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import sqlite3
 
 class Browser(QtWidgets.QWidget, BrowserUI.Ui_Form):
     def __init__(self):
+        global sessionSettings
         super().__init__()
         self.webPage = QWebEngineView()
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.webPage)
         self.setupUi(self)
         self.SetUpFunctions()
-        self.searchStart = "https://google.com/search?q="
+        if int(sessionSettings[0][2]) == 0:
+            self.searchStart = "https://google.com/search?q="
+        elif int(sessionSettings[0][2]) == 1:
+            self.searchStart = "https://ya.ru/search/?text="
 
     def SetUpFunctions(self):
         self.urlLineEdit.returnPressed.connect(self.OnEnterPressed)
@@ -23,27 +27,44 @@ class Browser(QtWidgets.QWidget, BrowserUI.Ui_Form):
         self.searchButton.clicked.connect(self.OnEnterPressed)
         self.newTabButton.clicked.connect(self.CreateNewWindow)
         self.menuButton.clicked.connect(self.OpenMenu)
+        self.addToFavoriteButton.clicked.connect(self.AddToFavorites)
+        self.searchHistoryList.itemDoubleClicked.connect(self.LoadFromSearchHistory)
+        self.LoadSearchHistory()
+        self.favoriteTab.setParent(None)
+        self.favoriteButton.setParent(None)
+        self.favoriteTab = QtWidgets.QToolBar(self)
+        self.gridLayout.addWidget(self.favoriteTab, 1, 0)
+        self.LoadFavoritesTab()
+        #self.favoriteTab.actionTriggered.connect(self.LoadFromFavoritesTab)
     
     def OnEnterPressed(self):
+        global sessionSettings, urlStarts
         self.webPage.setParent(None)
         self.webPage = QWebEngineView()
         self.gridLayout.addWidget(self.webPage, 2, 0)
 
         self.webPage.urlChanged.connect(self.UrlChanged)
 
+        if int(sessionSettings[0][2]) == 0:
+            self.searchStart = "https://google.com/search?q="
+        elif int(sessionSettings[0][2]) == 1:
+            self.searchStart = "https://ya.ru/search/?text="
+
         if (self.sender().objectName() == "urlLineEdit"):
-            if ("https://" == self.urlLineEdit.text()[:8]):
-                self.webPage.setUrl(QUrl(self.urlLineEdit.text()))
-            else:
-                self.webPage.setUrl(QUrl(self.searchStart + self.urlLineEdit.text()))
+            url = self.CheckUrlStart(self.urlLineEdit.text())
         else:
-            if ("https://" == self.urlLineEdit2.text()[:8]):
-                self.webPage.setUrl(QUrl(self.urlLineEdit2.text()))
-            else:
-                self.webPage.setUrl(QUrl(self.searchStart + self.urlLineEdit2.text()))
+            url = self.CheckUrlStart(self.urlLineEdit2.text())
         
+        self.webPage.setUrl(QUrl(url[0]))
+        self.urlLineEdit.setText(url[0])
+
+        if bool(sessionSettings[2][2] and url[1] != None):
+            conn = sqlite3.connect("browser.db")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO searchHistory (query) VALUES (?)", (url[1], ))
+            conn.commit()
+            conn.close()
         
-    
     def CreateNewWindow(self):
         global windows
         newWindow = Browser()
@@ -64,12 +85,15 @@ class Browser(QtWidgets.QWidget, BrowserUI.Ui_Form):
         self.menu.show()
     
     def UrlChanged(self):
+        global sessionSettings
         print("CHANGED")
-        conn = sqlite3.connect('browser.db')
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO browserHistory (url, title) VALUES (?, ?)', (self.webPage.url().toString(), self.webPage.title()))
-        conn.commit()
-        conn.close()
+        self.urlLineEdit.setText(self.webPage.url().toString())
+        if bool(sessionSettings[1][2]):
+            conn = sqlite3.connect('browser.db')
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO browserHistory (url, title) VALUES (?, ?)', (self.webPage.url().toString(), self.webPage.title()))
+            conn.commit()
+            conn.close()
     
     def LoadUrl(self, url):
         self.webPage.setParent(None)
@@ -79,6 +103,63 @@ class Browser(QtWidgets.QWidget, BrowserUI.Ui_Form):
         self.webPage.urlChanged.connect(self.UrlChanged)
         
         self.webPage.setUrl(QUrl(url))
+    
+    def CheckUrlStart(self, prompt):
+        global urlStarts
+        if any([urlStart in prompt for urlStart in urlStarts]):
+            return (prompt, None)
+        else:
+            return (self.searchStart + prompt, prompt)
+    
+    def LoadSearchHistory(self):
+        conn = sqlite3.connect("browser.db")
+        cursor = conn.cursor()
+        searchHistory = list(cursor.execute("SELECT query FROM searchHistory"))
+        conn.close()
+        for item in searchHistory:
+            self.searchHistoryList.addItem(item[0])
+    
+    def LoadFromSearchHistory(self):
+        self.webPage.setParent(None)
+        self.webPage = QWebEngineView()
+        self.gridLayout.addWidget(self.webPage, 2, 0)
+
+        self.webPage.urlChanged.connect(self.UrlChanged)
+        
+        self.webPage.setUrl(QUrl(self.searchStart + self.searchHistoryList.currentItem().text()))
+
+    def LoadFavoritesTab(self):
+        conn = sqlite3.connect("browser.db")
+        cursor = conn.cursor()
+        favorites = list(cursor.execute("SELECT * FROM favorites"))
+        conn.close()
+        for item in favorites:
+            action = QtWidgets.QWidgetAction(self)
+            action.setText(item[1])
+            action.triggered.connect(lambda: self.LoadFromFavoritesTab(item[2]))
+            self.favoriteTab.addAction(action)
+    
+    def AddToFavorites(self):
+        print("ADDED")
+        conn = sqlite3.connect("browser.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO favorites (favoriteName, favoriteUrl) VALUES (?, ?)", (self.webPage.page().title(), self.webPage.url().toString()))
+        conn.commit()
+        conn.close()
+    
+    def LoadFromFavoritesTab(self, url):
+        self.webPage.setParent(None)
+        self.webPage = QWebEngineView()
+        self.gridLayout.addWidget(self.webPage, 2, 0)
+
+        self.webPage.urlChanged.connect(self.UrlChanged)
+        
+        print(url)
+        self.webPage.setUrl(QUrl(url))
+
+
+        
+
 
 
 class Menu(QtWidgets.QWidget, MenuUI.Ui_Form):
@@ -109,14 +190,24 @@ class Menu(QtWidgets.QWidget, MenuUI.Ui_Form):
         self.close()
     
     def Save(self):
-        ### СОХРАНЕНИЕ НАСТРОЕК ###
-        pass
+        global sessionSettings
+        conn = sqlite3.connect("browser.db")
+        cursor = conn.cursor()
+        cursor.execute("UPDATE settings SET settingValue = ? WHERE settingName = 'searchSystem'", (self.searchSystem.currentIndex(), ))
+        cursor.execute("UPDATE settings SET settingValue = ? WHERE settingName = 'saveBrowserHistoryFlag'", (self.saveBrowserHistoryFlag.isChecked(), ))
+        cursor.execute("UPDATE settings SET settingValue = ? WHERE settingName = 'saveSearchHistoryFlag'", (self.saveSearchHistoryFlag.isChecked(), ))
+        conn.commit()
+        sessionSettings = list(cursor.execute("SELECT * FROM settings"))
+        conn.close()
 
     def LoadSettingsIntoUI(self):
-        self.searchSystem.setCurrentIndex(0)
-        self.saveBrowserHistoryFlag.setChecked(False)
-        self.saveSearchHistoryFlag.setChecked(False)
-        ### ЗДЕСЬ НУЖНО ЗАГРУЖАТЬ СОХРАНЕННЫЕ НАСТРОЙКИ В UI, НАПРИМЕР КАК В СТРОЧКАХ ВЫШЕ, ТОЛЬКО НУЖНО БРАТЬ АРГУМЕНИЫ ИЗ СОХРАНЕНИЯ ###
+        conn = sqlite3.connect("browser.db")
+        cursor = conn.cursor()
+        settings = list(cursor.execute("SELECT * FROM settings"))
+        self.searchSystem.setCurrentIndex(int(settings[0][2]))
+        self.saveBrowserHistoryFlag.setChecked(bool(settings[1][2]))
+        self.saveSearchHistoryFlag.setChecked(bool(settings[2][2]))
+        conn.close()
     
     def ClearBrowserHistory(self):
         clearDialog = ClearDialog()
@@ -172,11 +263,12 @@ class SearchHistory(QtWidgets.QWidget, HistoryUI.Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.SetUpFunctions()
-        self.LoadSearchHistory()
     
     def SetUpFunctions(self):
         self.backButton.clicked.connect(self.Close)
         self.historyName.setText("История поиска")
+        self.LoadSearchHistory()
+        self.historyList.itemDoubleClicked.connect(self.OpenPage)
     
     def Close(self):
         self.close()
@@ -214,11 +306,12 @@ class ClearDialog(QtWidgets.QDialog):
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
 
-def create_tables():
+
+def CreateTables():
     conn = sqlite3.connect('browser.db')
     cursor = conn.cursor()
 
-    # история поиска
+    # История поиска
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS searchHistory (
             id INTEGER PRIMARY KEY,
@@ -227,7 +320,7 @@ def create_tables():
         )
     ''')
 
-    # общая история
+    # История посещений
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS browserHistory (
             id INTEGER PRIMARY KEY,
@@ -242,7 +335,16 @@ def create_tables():
         CREATE TABLE IF NOT EXISTS settings (
             id INTEGER PRIMARY KEY,
             settingName TEXT NOT NULL,
-            settingValue TEXT NOT NULL
+            settingValue INTEGER
+        )
+    ''')
+
+    # Закладки
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS favorites (
+            id INTEGER PRIMARY KEY,
+            favoriteName TEXT NOT NULL,
+            favoriteUrl TEXT NOT NULL
         )
     ''')
 
@@ -260,8 +362,19 @@ if __name__ == "__main__":
     (0, "searchSystem", 0),
     (1, "saveBrowserHistoryFlag", 1),
     (2, "saveSearchHistoryFlag", 1)]
+    urlStarts = [
+    "https://",
+    "http://",
+    "www."]
+    
+    CreateTables()
+
+    conn = sqlite3.connect("browser.db")
+    cursor = conn.cursor()
+    sessionSettings = list(cursor.execute("SELECT * FROM settings"))
+    conn.close()
+
     browser = Browser()
     windows.append(browser)
     windows[0].show()
-    create_tables()
     app.exec()
